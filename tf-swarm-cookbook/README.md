ff0a1bd147e77b3f2fb9fba940270c17059fecce
Adam Patterson <adamthepatterson@gmail.com>
2016-01-16 22:44:37 -0800

    first

---
+++ tf-swarm-cookbook/README.md
@@ -0,0 +1,4 @@
+# tf-swarm-cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ tf-swarm-cookbook/metadata.rb
@@ -0,0 +1,9 @@
+name 'tf-swarm-cookbook'
+maintainer 'adampatts'
+maintainer_email 'adamthepatterson@gmail.com'
+license 'MIT'
+description 'Installs/Configures tf-swarm-cookbook'
+long_description 'Installs/Configures tf-swarm-cookbook'
+version '0.1.0'
+
+depends 'docker', '~> 2.4.1'
