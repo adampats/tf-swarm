fb6edd4e73769d0bb7693c80bb7f41ca6519ccde
Adam Patterson <adamthepatterson@gmail.com>
2016-01-17 14:53:26 -0800

    forgot attrs

---
+++ tf-swarm-cookbook/attributes/default.rb
@@ -0,0 +1,6 @@
+
+default['tf-swarm-cookbook']['packages'] = [ "curl", "jq", "awscli" ]
+default['tf-swarm-cookbook']['user'] = 'ubuntu'
+
+default['tf-swarm-cookbook']['docker']['version'] = '1.9.1'
+default['tf-swarm-cookbook']['swarm']['version'] = 'latest'
