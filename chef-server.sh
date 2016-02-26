587989f63298f819dddefe1e708b2ee49f67eb52
Adam Patterson <adamthepatterson@gmail.com>
2016-02-26 00:19:02 -0800

    chef server script

---
+++ chef-server.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# parameters
+chef_file='chef-server-core_12.4.1-1_amd64.deb'
+user='chef'
+pass='password'
+key_file='/home/ubuntu/chef.pem'
+org_name='cheforg'
+chef_manage='true'
+chef_push='true'
+chef_reporting='false'
+
+# script
+curl -L -s -k "https://packagecloud.io/chef/stable/packages/ubuntu/trusty/$chef_file/download" \
+  -o "/tmp/$chef_file"
+
+dpkg -i "/tmp/$chef_file"
+
+chef-server-ctl reconfigure
+
+chef-server-ctl user-create \
+  $user 'chef' 'admin' 'none@none.com' $pass --filename $key_file
+
+chef-server-ctl org-create \
+  $org_name 'Chef Server' --association_user $user --filename $key_file
+
+if [ $chef_manage == 'true' ]; then
+  chef-server-ctl install chef-manage
+  chef-server-ctl reconfigure
+  chef-manage-ctl reconfigure
+fi
+
+if [ $chef_push == 'true' ]; then
+  chef-server-ctl install opscode-push-jobs-server
+  chef-server-ctl reconfigure
+  opscode-push-jobs-server-ctl reconfigure
+fi
+
+if [ $chef_reporting == 'true' ]; then
+  chef-server-ctl install opscode-reporting
+  chef-server-ctl reconfigure
+  opscode-reporting-ctl reconfigure
+fi
+
+echo 'Done.'
